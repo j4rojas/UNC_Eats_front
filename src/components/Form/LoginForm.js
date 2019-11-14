fac2099cc801ca4483400cdb783dab0c7b9e7868
Jasmine <jasminerojas102@gmail.com>
2019-11-14 12:46:37 -0500

    change localhost to heroku

--- src/components/Form/LoginForm.js
+++ src/components/Form/LoginForm.js
@@ -22,7 +22,7 @@ export default class LoginForm extends React.Component {
     
     loginSubmit(event) {
         event.preventDefault();
-        fetch('http://localhost:8080/user/login', {
+        fetch('https://unc-eats-back.herokuapp.com/user/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
